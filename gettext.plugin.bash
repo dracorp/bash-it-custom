36815fd192112846ccae1f8a81d76976a3c1e35a
Piotr Rogoza <rogoza.piotr@gmail.com>
2020-08-03 11:37:06 +0200

    Workaround for util-linux and gettext

--- gettext.plugin.bash
+++ gettext.plugin.bash
@@ -1,9 +1,11 @@
-# gettext is keg-only, which means it was not symlinked into /usr/local,
-# because macOS provides the BSD gettext library & some software gets confused if both are in the library path.
+if [ -d /usr/local/opt/gettext ]; then
+    # gettext is keg-only, which means it was not symlinked into /usr/local,
+    # because macOS provides the BSD gettext library & some software gets confused if both are in the library path.
 
-# If you need to have gettext first in your PATH run:
-_add2env PATH "/usr/local/opt/gettext/bin"
+    # If you need to have gettext first in your PATH run:
+    _add2env PATH "/usr/local/opt/gettext/bin"
 
-# For compilers to find gettext you may need to set:
-_add2env LDFLAGS "-L/usr/local/opt/gettext/lib" " "
-_add2env CPPFLAGS "-I/usr/local/opt/gettext/include" " "
+    # For compilers to find gettext you may need to set:
+    _add2env LDFLAGS "-L/usr/local/opt/gettext/lib" " "
+    _add2env CPPFLAGS "-I/usr/local/opt/gettext/include" " "
+fi
--- util-linux.plugin.bash
+++ util-linux.plugin.bash
@@ -1,7 +1,9 @@
-_add2env PATH=+"/usr/local/opt/util-linux/bin"
-_add2env PATH=+"/usr/local/opt/util-linux/sbin"
+if [ -d /usr/local/opt/util-linux ]; then
+    _add2env PATH=+"/usr/local/opt/util-linux/bin"
+    _add2env PATH=+"/usr/local/opt/util-linux/sbin"
 
-_add2env LDFLAGS "-L/usr/local/opt/util-linux/lib" " "
-_add2env CPPFLAGS "-I/usr/local/opt/util-linux/include" " "
+    _add2env LDFLAGS "-L/usr/local/opt/util-linux/lib" " "
+    _add2env CPPFLAGS "-I/usr/local/opt/util-linux/include" " "
 
-_add2env PKG_CONFIG_PATH "/usr/local/opt/util-linux/lib/pkgconfig"
+    _add2env PKG_CONFIG_PATH "/usr/local/opt/util-linux/lib/pkgconfig"
+fi
