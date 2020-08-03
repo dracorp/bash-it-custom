b9604d2731c342937b26bfbe0ef8538ce4aa29f5
Piotr Rogoza <rogoza.piotr@gmail.com>
2020-08-03 11:34:05 +0200

    Workaround for ls and openjdk

--- ls.plugin.bash
+++ ls.plugin.bash
@@ -2,7 +2,7 @@ if which dircolors 1>/dev/null 2>&1; then
      test -r ~/.dir_colors && eval "$(dircolors -b ~/.dir_colors)" || eval "$(dircolors -b)"
 fi
 
-if [[ $(uname) = Linux || $(which ls | xargs dirname) = /usr/local/opt/coreutils/libexec/gnubin ]]; then
+if [[ $(uname) = Linux || $(which ls | grep -v alias | xargs dirname) = /usr/local/opt/coreutils/libexec/gnubin ]]; then
     alias ls='ls -Fh --group-directories-first --color=auto --time-style="+%Y-%m-%d %H:%M:%S"'
     alias ll='ls -lFh --group-directories-first --color=auto --time-style="+%Y-%m-%d %H:%M:%S"'
     alias l.='ls -d .* --group-directories-first --color=auto'
@@ -14,7 +14,8 @@ else
 fi
 
 # https://nfsec.pl/1line/6154
-_rm4env LS_COLORS 'ex=01;31' :
-_add2env LS_COLORS 'ex=00:su=00:sg=00:ca=00' :
+# TODO:
+# _rm4env LS_COLORS 'ex=01;31' :
+# _add2env LS_COLORS 'ex=00:su=00:sg=00:ca=00' :
 export LC_COLLATE=C
 export LANG=C
--- openjdk.plugin.bash
+++ openjdk.plugin.bash
@@ -1,3 +1,6 @@
 _add2env PATH=+"/usr/local/opt/openjdk/bin"
-_add2env CPPFLAGS "-I/usr/local/opt/openjdk/include" " "
+# TODO: -bash: -I/usr/local/opt/openjdk/include: No such file or directory
+if [ -d /usr/local/opt/openjdk/include ]; then
+    _add2env CPPFLAGS "-I/usr/local/opt/openjdk/include" " "
+fi
 export JAVA_HOME="/usr/local/opt/openjdk"
