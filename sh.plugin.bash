3494d7c70f2563f035a43ebfd863e18c77366f98
Piotr Rogoza <rogoza.piotr@gmail.com>
2021-03-08 12:59:06 +0100

    Added fallback for bash-it and sh files

---
+++ sh.plugin.bash
@@ -0,0 +1,18 @@
+# by default bash-it does not read *.sh files, only *.bash
+get_sh_directory() {
+    local SOURCE DIR
+    SOURCE="${BASH_SOURCE[0]}"
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    echo $DIR
+}
+CUSTOM_DIRECTORY=$(get_sh_directory)
+if [[ -n $CUSTOM_DIRECTORY && -d $CUSTOM_DIRECTORY ]]; then
+    for sh_file in $CUSTOM_DIRECTORY/*.sh; do
+        source $sh_file
+    done
+fi
