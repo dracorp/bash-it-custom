70796cf0928ab63252bf25049b47159a652fd806
Piotr Rogoza <rogoza.piotr@gmail.com>
2021-05-31 13:38:29 +0200

    Changed settings for log4sh

--- 03-log4sh.plugin.sh
+++ 03-log4sh.plugin.sh
@@ -1,4 +1,5 @@
-log4sh="$(basher package-path dracorp/log4sh)/log4sh.sh"
-if [[ -r $log4sh ]]; then
-    . $log4sh
-fi
+# log4sh="$(basher package-path dracorp/log4sh)/log4sh.sh"
+# if [[ -r $log4sh ]]; then
+#     . $log4sh
+# fi
+include dracorp/log4sh log4sh.sh
